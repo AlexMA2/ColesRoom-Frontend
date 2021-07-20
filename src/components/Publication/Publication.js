861af47462f722a5a86ce43d2d575b944e4416f8
Alex <58560420+AlexMA2@users.noreply.github.com>
2021-07-20 11:10:02 -0500

    Creada la estructura HTML de la publicación

--- src/components/Publication/Publication.js
+++ src/components/Publication/Publication.js
@@ -1,22 +1,112 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import DescriptionIcon from '@material-ui/icons/Description';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 import 'draft-js/dist/Draft.css';
 
-const Publication = ({p}) => {    
+const Publication = ({ p }) => {
+
+    const [openEdit, setOpenEdit] = useState(false);
+    const [openDelete, setOpenDelete] = useState(false);
+
+    const handleClickOpenEdit = () => {
+        setOpenEdit(true);
+    };
+
+    const handleCloseEdit = () => {
+        setOpenEdit(false);
+    };
+
+    const handleClickOpenDelete = () => {
+        setOpenDelete(true);
+    };
+
+    const handleCloseDelete = () => {
+        setOpenDelete(false);
+    };
+
+
 
     return (
         <div className="publication">
+            <Dialog open={openEdit} onClose={handleCloseEdit} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title"> Editar publicación </DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Elimina archivos pulsando en la X de cada archivo.
+                        Sube los archivos pulsando en el botón "Subir".
+                        Para que se muestren los cambios pulsa el botón "Guardar Cambios".
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Edita la publicación"
+                        type="email"
+                        fullWidth
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseEdit} color="primary">
+                        Cancelar
+                    </Button>
+                    <Button onClick={handleCloseEdit} color="primary">
+                        Subir archivos
+                    </Button>
+                    <Button onClick={handleCloseEdit} color="primary">
+                        Guardar Cambios
+                    </Button>
+                </DialogActions>
+            </Dialog>
+            <Dialog
+                open={openDelete}
+                onClose={handleCloseDelete}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        ¿Est&aacute;s seguro de querer eliminar esta publicaci&oacute;n?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseDelete} color="primary">
+                        No, no lo elimines.
+                    </Button>
+                    <Button onClick={handleCloseDelete} color="primary" autoFocus>
+                        Sí, elim&iacute;nalo.
+                    </Button>
+                </DialogActions>
+            </Dialog>
+            <div className="publication__controls">
+                <Button variant="outlined" color="primary" onClick={handleClickOpenEdit}>
+                    Editar
+                </Button>
+                <Button variant="outlined" color="primary" onClick={handleClickOpenDelete}>
+                    Eliminar
+                </Button>
+            </div>
+            <div className="publication__date">
+                {/* {p.datePublicated} */}
+                Creado hace 2 dias - 18/02/2019 07:00:05
+            </div>
             <div className="publication__content">
                 {p.content}
             </div>
             <div className="publication__files">
                 {p.route.map((path, index) =>
                     <div className="publication__file" key={index}>
-                        <a href={path} target="_blank">{<DescriptionIcon fontSize="large"/>}</a>
+                        <a href={path} target="_blank">{<DescriptionIcon fontSize="large" />}</a>
                     </div>
-                )}            
-            </div>            
+                )}
+            </div>
         </div>
     )
 }
