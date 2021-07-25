e0fde382ef80102d315c560fc464e7def5867652
Alex <58560420+AlexMA2@users.noreply.github.com>
2021-07-24 23:10:17 -0500

    Agregado funcion en interfaz de editar. Fin de la rama

--- src/components/CourseTitle/CourseTitle.js
+++ src/components/CourseTitle/CourseTitle.js
@@ -1,12 +1,22 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import './CourseTitle.css'
 
 import DefaultBackground from '../../imgs/CourseBackgroundDefault.jpg'
 import CreateIcon from '@material-ui/icons/Create';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 
 const CourseTitle = ({ name, description, date, backgroundImage }) => {
 
     const [dateFormat, setdateFormat] = useState('')
+    const [editing, setediting] = useState(false)
+    const [heightTitle, setheightTitle] = useState(0)
+    const [heightContent, setheighContent] = useState(0)
+
+    const [newTitle, setnewTitle] = useState('')
+    const [newContent, setnewContent] = useState('')
+
+    const titleRef = useRef();
+    const contentRef = useRef();
 
     const backgroundCourse = {
         backgroundImage: backgroundImage ? `url(${backgroundImage})` : `url(${DefaultBackground})`,
@@ -28,24 +38,87 @@ const CourseTitle = ({ name, description, date, backgroundImage }) => {
                 dateToFormat.getUTCHours(),
                 dateToFormat.getUTCMinutes(),
                 dateToFormat.getUTCSeconds()));
-            
-            setdateFormat(df.format(dateTransform) )
+
+            setdateFormat(df.format(dateTransform))
         }
 
     }, [date])
+
+    const setStylesInputTitle = (newHeight) => {
+        return {
+            height: newHeight + 'px',
+            width: '100%',
+            fontSize: '2.25rem',
+            fontWeight: 'bold',
+            fontFamily: '"Google Sans", Roboto, Arial, sans-serif',
+            background: 'none',
+            color: '#fff',
+            border: 'none',
+            borderBottom: '1px solid #4f4f4f'
+        }
+    }
+
+    const setStylesInputContent = (newHeight) => {
+        return {
+            height: (newHeight + 10) + 'px',
+            width: '100%',
+            fontSize: '1rem',                        
+            background: 'none',
+            color: '#fff',
+            border: 'none',
+            borderBottom: '1px solid #4f4f4f',
+            paddingBottom: '0.5rem',
+        }
+    }
+
+    const handleOpenEditCourse = (ev) => {       
+        setheightTitle(titleRef.current.offsetHeight)   
+        setnewTitle(name)  
+        setheighContent(contentRef.current.offsetHeight)   
+        setnewContent(description)
+        titleRef.current.focus()
+        setediting(true)
+    }
+
+    const handleCloseEditCourse = (ev) => {
+        setediting(false)
+    }
+
+    const handleChangeTitle = (ev) => {
+        setnewTitle(ev.target.value)
+    }
+
+    const handleChangeContent = (ev) => {
+        setnewContent(ev.target.value)
+        
+    }
 
     return (
         <div className="course-title" style={backgroundCourse}>
             <div className="course-title__info">
                 <div className="course__subinfo">
-                    <h2 > {name} </h2>
+                    {
+                        editing 
+                        ? <input type="text" value={newTitle} style={setStylesInputTitle(heightTitle)} onChange={handleChangeTitle}/>
+                        : <h2 ref={titleRef}> {name} </h2>
+                    }
                     <div>
-                        <CreateIcon />
-                    </div>                    
+                        {
+                            editing 
+                            ? <HighlightOffIcon onClick={handleCloseEditCourse} />
+                            : <CreateIcon onClick={handleOpenEditCourse}/>
+                        }
+                       
+                    </div>
                 </div>
                 <div className="course__description">
+                    
                     <p> Creado el {dateFormat} </p>
-                    {description}
+                    {
+                        editing 
+                        ? <textarea style={setStylesInputContent(heightContent)} onChange={handleChangeContent}>{newContent}</textarea>
+                        : <span ref={contentRef}>{description}</span>
+                    }
                 </div>
             </div>
 
