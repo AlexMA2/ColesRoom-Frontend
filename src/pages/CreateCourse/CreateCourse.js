ee853c4a1369a582c0546cab84f31684cecc780e
AlexisRM0511 <alexis.rojas1@unmsm.edu.pe>
2021-08-01 14:00:33 -0500

    wef

--- src/pages/CreateCourse/CreateCourse.js
+++ src/pages/CreateCourse/CreateCourse.js
@@ -37,7 +37,7 @@ const CreateCourse = (props) => {
       }
     })
       .then(res => res.json())
-      .then()
+      .then(alert("LOGUEO CORRECTO"))
       .catch(err => alert("Fallo al Registrar"));
 
     history.push("/");
